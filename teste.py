cf5721b87da52191ba5a29e11f487273b730b58c
Salvador-Melo <salvador.melo@gmail.com>
2026-03-05 18:29:56 -0300

    remove Flask app and add MySQL connection script

--- teste.py
+++ teste.py
@@ -1,16 +1,3 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.get("/")
-def cliente():
-    dados = {
-        "id": 1,
-        "nome": "Ana",
-        "ativo": True
-    }
-    return jsonify(dados)
-if __name__ == "__main__":
-    app.run(debug=True)
-
+import pymysql
+engine = create_engine(DATABASE_URL)
 
