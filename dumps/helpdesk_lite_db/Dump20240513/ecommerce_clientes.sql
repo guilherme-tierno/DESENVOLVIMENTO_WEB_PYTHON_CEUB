f0290053f61f58eb619558d2ea2ff107859101e4
Salvador-Melo <salvador.melo@gmail.com>
2026-03-05 20:42:53 -0300

    dumps

---
+++ dumps/helpdesk_lite_db/Dump20240513/ecommerce_clientes.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: ecommerce
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.28-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Patrick Green','markstanley@example.net'),(2,'Jennifer Diaz','joneskevin@example.net'),(3,'Jacob Mullins','tyler04@example.org'),(4,'Ruth Davis','joshua12@example.com'),(5,'Lauren Hansen','romerowendy@example.net'),(6,'Melissa Adams','patrick46@example.com'),(7,'Evelyn Lee','bchristian@example.com'),(8,'Kristen Ortiz','brussell@example.com'),(9,'Mark Scott','keith45@example.com'),(10,'Brandon Calhoun','ysmith@example.net'),(11,'Alyssa Blackwell','xayers@example.com'),(12,'Cody Guzman','beverly98@example.com'),(13,'Autumn Stephens','juarezjohn@example.org'),(14,'Michelle Jones','waynejoseph@example.org'),(15,'Justin Rich','wjohnson@example.org'),(16,'Juan Price','gomezlinda@example.net'),(17,'Jasmin Glenn','hilllauren@example.org'),(18,'Keith Costa','lindseyashley@example.net'),(19,'Dustin Burton','chaddaniels@example.com'),(20,'Raymond Owens','kendra94@example.com'),(21,'Elizabeth Romero','mclaughlinkyle@example.org'),(22,'Amanda Bird','tim53@example.com'),(23,'Gregory Gordon','karlabrown@example.net'),(24,'Susan Taylor','wendy83@example.org'),(25,'Troy Lawson MD','hughessarah@example.net'),(26,'Traci White','watsonalbert@example.com'),(27,'Isaac Lynch','zmarquez@example.org'),(28,'Lisa Maldonado','julie75@example.com'),(29,'Johnny Allen','jshaw@example.com'),(30,'Darrell Morris','tracy79@example.org');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-13 15:25:06
